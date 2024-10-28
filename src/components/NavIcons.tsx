536cef7e0a423e447359bcdc5ae6db3d2feb3374
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-10-28 14:33:48 +0100

    working on the navbar components and the cart count

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const NavIcons = () => {
-  return (
-    <div>NavIcons</div>
-  )
-}
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCarteOpen] = useState(false);
 
-export default NavIcons
+  const router = useRouter();
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if(isLoggedIn)
+      router.push("/Login")
+
+    setIsProfileOpen((prev) => !prev);
+
+  }
+  return (
+    <div className="flex items-center justify-between gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={24}
+        height={24}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen &&(
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-lg">
+          <Link href="/">Profile</Link>
+          <div className="mt-3 cursor-pointer">Logout</div>
+        </div>
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={24}
+        height={24}
+        className="cursor-pointer"
+      />
+      <Image
+        src="/cart.png"
+        alt=""
+        width={24}
+        height={24}
+        className="cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default NavIcons;

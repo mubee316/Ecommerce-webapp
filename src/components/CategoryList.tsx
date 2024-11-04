2e3cbc8efc4d105fea1dee27a8f2584670b5d536
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-11-04 22:09:45 +0100

    worked oon the category products and also gave it a feature to scroll horzontally

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,94 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const CategoryList = () => {
+  return (
+    <div className='px-4 overflow-x-scroll scrollbar'>
+        <div className=' relative flex gap-4 md:gap-8'>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+            <Link href='/test?cat=test' className='flex-shrink-0 w-full sm:w-1/2 md:w-1/4 lg:w-1/6'>
+            <div className='relative bg-slate-100 w-full h-96'>
+            <Image src="/pixel.jpg"
+          alt=''
+          fill
+          sizes='25vw'
+          className='absolute rounded-md'/>
+          </div>
+          <h1 className='mt-8 font-light text-cl tracking-wide '>Category Name</h1>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryList

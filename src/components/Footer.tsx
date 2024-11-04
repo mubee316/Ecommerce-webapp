45588963fc971accf5f256a0f7cdee6619d26dcb
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-11-05 00:09:42 +0100

    stll on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,41 @@ const Footer = () => {
 
           </div>
         </div> 
-        <div className=" hidden lg:flex justify-between gap-8 w-1/2"></div>
+        <div className=" hidden lg:flex justify-between gap-8 w-1/2">
+        <div className="flex flex-col justify-between">
+          <h1 className="font-semibold">COMPANY</h1>
+          <div className="flex flex-col gap-6">
+            <Link href="/">About us</Link>
+            <Link href="">Careers</Link>
+            <Link href="/">Affiliate</Link>
+            <Link href="/">Blog</Link>
+            <Link href="/">Contact Us</Link>
+
+          </div>
+          </div>
+        <div className="flex flex-col justify-between">
+          <h1 className="font-semibold">SHOP</h1>
+          <div className="flex flex-col gap-6">
+            <Link href="/">New Arrivals</Link>
+            <Link href="">Accessories</Link>
+            <Link href="/">Men</Link>
+            <Link href="/">Women</Link>
+            <Link href="/">All Products</Link>
+
+          </div>
+          </div>
+        <div className="flex flex-col justify-between">
+          <h1 className="font-semibold">HELP</h1>
+          <div className="flex flex-col gap-6">
+            <Link href="/">Customer Service</Link>
+            <Link href="">My Account</Link>
+            <Link href="/">Find a Store</Link>
+            <Link href="/">Legal & Privacy</Link>
+            <Link href="/">Gift Card</Link>
+
+          </div>
+          </div>
+          </div>
         <div className=" w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
         <h1 className="font-medium text-lg">SUBSCRIBE</h1>
         <p>

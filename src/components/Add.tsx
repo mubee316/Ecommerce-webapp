cb7bea3cd6968a136a0c16dc844b75bf4ee94bee
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-11-16 00:53:21 +0100

    dont with the product page now moving to authentication

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Add = () => {
+
+    // tempo
+    const stock = 4;
+    const [Quantity , setQuantity] = useState(1);
+
+    const handleQuantity = (type: "i" | "d") => {
+        if(type === "d" && Quantity > 1){
+            setQuantity((prev) => prev - 1)
+            
+        }
+        if(type ==="i" && Quantity < stock){
+            setQuantity((prev) => prev + 1)
+        }
+    }
+
   return (
-    <div>Add</div>
+    <div className='flex flex-col gap-6'>
+        <h3>Choose a Quantity</h3>
+        <div className='flex justify-between'>
+            <div className='flex gap-6'>
+                <div className='bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-36'>
+                    <button className='cursor-pointer text-xl' onClick={() => handleQuantity("d")}>-</button>
+                    {Quantity}
+                    <button className='cursor-pointer text-xl ' onClick={() => handleQuantity("i")}>+</button>
+                </div>
+                <div className='font-medium text-sm'>
+                    Only <span className='text-orange-500'>4 items</span> left !! <br />Don&apos;t miss it
+                </div>
+            </div>
+            <button className='ring-1 ring-pink-500 rounded-md py-1 px-4 text-pink-500 hover:bg-pink-500 hover:text-white text-sm disabled:bg-pink-200 disabled:text-white disabled:cursor-not-allowed'> Add to Cart</button>
+        </div>
+    </div>
   )
 }
 
--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CustomizeProducts = () => {
   return (
     <div>
-        <div>
+        <div className='flex flex-col gap-6'>
             <h3>Choose a color</h3>
             <ul className='flex items-center gap-4'>
                 <li className='w-8 h-8 rounded-full bg-red-500 ring-1 ring-gray-300 cursor-pointer relative'>
@@ -15,9 +15,15 @@ const CustomizeProducts = () => {
                     <div className='absolute w-10 h-[2px] bg-red-400 rotate-45  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/>
                 </li>
             </ul>
+        <h3>Choose a Size</h3>
+       
+            <ul className='flex items-center gap-6'>
+                <li className='ring-1 ring-pink-500  text-pink-500 rounded-md px-4 py-1 cursor-pointer text-sm w-max '>Small</li>
+                <li className='ring-1 ring-pink-500  text-white bg-pink-500 rounded-md px-4 py-1 cursor-pointer text-sm w-max '>Medium</li>
+                <li className='ring-1 ring-pink-200  text-white bg-pink-200 cursor-not-allowed rounded-md px-4 py-1  text-sm w-max '>Large</li>
+            </ul>
         </div>
-        <div></div>
-    </div>
+        </div>
   )
 }
 
